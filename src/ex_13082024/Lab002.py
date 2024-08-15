204981808f87412939133784911bbd6c124a2a09
LENOVO <pallagururaj@gmail.com>
2024-08-15 23:53:41 +0530

    Task

---
+++ src/ex_13082024/Lab002.py
@@ -0,0 +1,26 @@
+# Task 2 - Assignment
+
+# Max Number
+num1 = int(input("Enter the first number"))
+num2 = int(input("Enter the second number"))
+print(max(num1,num2))
+
+# Power
+print(pow(num1,num2))
+
+# Addition
+sum = num1+num2
+print(f"{sum:.3f}")
+
+# Subtraction
+sub = num1-num2
+print(f"{sub:.3f}")
+
+# Division
+div = num1/num2
+print(f"{div:.3f}")
+
+# multiplication
+mul = num1*num2
+print(f"{mul:.3f}")
+
